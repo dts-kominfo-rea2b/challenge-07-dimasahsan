14ad4ec74bc319b4de413f29b9c0f8b4208c7194
Dimas Maulana Ahsan <dimasmaulanaahsan@gmail.com>
2022-06-12 13:22:13 +0700

    Solve promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const hasilPromiseIXX = await promiseTheaterIXX();
+  const hasilPromiseVGC = await promiseTheaterVGC();
+
+  const hasilIXX = hitung(hasilPromiseIXX, emosi);
+  const hasilVGC = hitung(hasilPromiseVGC, emosi);
+
+  return hasilIXX + hasilVGC;
+};
+
+const hitung = (hasilSetalahMenonton, emosi) => {
+  let total = 0;
+  hasilSetalahMenonton.forEach((item) => {
+    if (item.hasil == emosi) {
+      total++;
+    }
+  });
+  return total;
+};
 
 module.exports = {
   promiseOutput,
